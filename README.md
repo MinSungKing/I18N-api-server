52085eb8b632087ac0089c188de807208817e58c
Minsung Kim <minsung.kim1006@gmail.com>
2019-08-27 15:32:09 +0900

    Init github repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# I18N-api-server
+API server for I18N service
+
+## Requirement
+Spring Boot 2.1.7
---
+++ src/main/java/com/mskim/i18l/I18NApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.mskim.i18l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18NApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18NApiServerApplication.class, args);
+	}
+
+}
