214bcbb1544b13cf1e74593b350e9b94f6a3c2b2
Minsung Kim <minsung.kim1006@gmail.com>
2019-08-28 23:50:43 +0900

    Add Basic Auth

--- src/main/java/com/mskim/i18l/config/SecurityConfig.java
+++ src/main/java/com/mskim/i18l/config/SecurityConfig.java
@@ -1,27 +1,52 @@
 package com.mskim.i18l.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Value("${websecurityconfig.global.password}")
+	private String password;
+
+	@Value("${websecurityconfig.global.username}")
+	private String userName;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.csrf().disable().authorizeRequests()
-			.antMatchers("/keys", "/keys/**", "/language_detect").permitAll()
-			.anyRequest().authenticated();
+
+		http.csrf().disable();
+
+		// All requests send to the Web Server request must be authenticated
+		http.authorizeRequests().anyRequest().authenticated();
+
+		// Use AuthenticationEntryPoint to authenticate user/password
+		http.httpBasic().authenticationEntryPoint(authEntryPoint);
 	}
 
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		String encrytedPassword = this.passwordEncoder().encode(password);
+		auth.inMemoryAuthentication().withUser(userName).password(encrytedPassword).roles("USER");
 	}
 }
