eeee8e06fa4a258dc92c271c27ea8aafeb62395e
Minsung Kim <minsung.kim1006@gmail.com>
2019-08-28 17:30:05 +0900

    Add error messages for response

--- src/main/java/com/mskim/i18l/controller/ApiController.java
+++ src/main/java/com/mskim/i18l/controller/ApiController.java
@@ -41,20 +41,30 @@ public class ApiController {
 	@GetMapping("/keys")
 	public ResponseEntity<Map<String,Object>> getKey(@RequestParam(value="name", required=false)String name) {
 		
-		logger.info("GET /keys name=" + name);
+		logger.info("GET /keys	name=" + name);
+		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
 		
 		if(name != null && !Pattern.matches(namePattern, name)) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
+			message = "Key with name [" + name + "] is not valid. Only small letters and dot(.) are allowed.";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.BAD_REQUEST);
 		}
 		
 		List<HashMap<String,Object>> keys = i18nService.getKeys(name);
 		
 		if(keys.isEmpty()) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
+			message = "Can not find any key with name [" + name + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.NOT_FOUND);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("keys", keys);
+		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
 	}
 	
@@ -63,48 +73,73 @@ public class ApiController {
 
 		logger.info("POST /keys	name=" + key.getName());
 		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+		
 		if(!Pattern.matches(namePattern, key.getName())) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
+			message = "name [" + key.getName() + "] is not valid. Only small letters and dot(.) are allowed.";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.BAD_REQUEST);
 		}
 		
 		List<HashMap<String,Object>> keys = i18nService.getKeys(key.getName());
 		if(!keys.isEmpty()) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.CONFLICT);
+			message = "name [" + key.getName() + "] already exists.";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.CONFLICT);
 		}
 		
 		KeyDto insertedKey = i18nService.addKey(key);
 		
 		if(insertedKey == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			message = "Failed to insert Key with name [" + key.getName() + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("key", insertedKey);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
 	}
 	
 	@PutMapping(value = "/keys/{keyId}")
-	public ResponseEntity<Map<String,Object>> updateCustomer(@PathVariable("keyId") int keyId, @RequestBody @Valid KeyDto key) {
+	public ResponseEntity<Map<String,Object>> updateKey(@PathVariable("keyId") int keyId, @RequestBody @Valid KeyDto key) {
 		
 		logger.info("PUT /keys	keyId=" + keyId + ", name=" + key.getName());
 		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+
 		if(!Pattern.matches(namePattern, key.getName())) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
+			message = "name [" + key.getName() + "] is not valid. Only small letters and dot(.) are allowed.";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.BAD_REQUEST);
 		}
 		
 		key.setId(keyId);
-		if(i18nService.getKeyById(key) == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
+		KeyDto existingKey = i18nService.getKeyById(key);
+		if(existingKey == null) {
+			message = "Can not find any key with keyId [" + keyId + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.NOT_FOUND);
 		}
 		
 		KeyDto updatedKey = i18nService.updateKey(key);
 
 		if(updatedKey == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			message = "Failed to update key with keyId [" + keyId + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("key", updatedKey);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
@@ -116,14 +151,22 @@ public class ApiController {
 		
 		logger.info("POST /keys/{keyId}/translations/{locale}	keyId=" + keyId + ", locale=" + locale + ", value=" + translation.getValue());
 
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+		
 		String detectedLocale = i18nService.getLanguageLocale(translation.getValue());
 		if(detectedLocale == null || !detectedLocale.equals(locale)) {
-			Map<String,Object> messageMap = new HashMap<String,Object>();
-			messageMap.put("message", "value and locale are not matched.");
+			message = "value [" + translation.getValue() + "] and locale [" + locale + "] are not matched.";
+			logger.info(message);
+			messageMap.put("message", message);
 			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.BAD_REQUEST);
 		}
 		
 		if(!validLocales.contains(locale)) {
+			message = "locale [" + locale + "] is not allowed. ['ko', 'en', 'ja'] are valid.";
+			logger.info(message);
+			messageMap.put("message", message);
 			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
 		}
 		
@@ -132,21 +175,22 @@ public class ApiController {
 		
 		TranslationDto existingTranslation = i18nService.getTranslationByKeyIdAndLocale(translation);
 		if(existingTranslation != null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.CONFLICT);
+			message = "Translation with keyId [" + keyId + "] and locale [" + locale + "] already exists.";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.CONFLICT);
 		}
 		
 		TranslationDto insertedTranslation = i18nService.addTranslation(translation);
 		
 		if(insertedTranslation == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			message = "Failed to insert Translation with keyId [" + keyId + "] and locale [" + locale + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("translation", insertedTranslation);
-		System.out.println("id : " + insertedTranslation.getId());
-		System.out.println("key_id : " + insertedTranslation.getKeyId());
-		System.out.println("locale : " + insertedTranslation.getLocale());
-		System.out.println("value : " + insertedTranslation.getValue());
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
 	}
@@ -156,13 +200,19 @@ public class ApiController {
 		
 		logger.info("GET /keys/{keyId}/translations	keyId=" + keyId);
 		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+		
 		List<HashMap<String,Object>> translations = i18nService.getTranslationsByKeyId(keyId);
 		
 		if(translations.isEmpty()) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
+			message = "Can not find any translation with keyId [" + keyId + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.NOT_FOUND);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("translations", translations);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
@@ -174,6 +224,10 @@ public class ApiController {
 		
 		logger.info("GET /keys/{keyId}/translations{locale}	keyId=" + keyId + ", locale=" + locale);
 
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+		
 		if(!validLocales.contains(locale)) {
 			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
 		}
@@ -185,10 +239,12 @@ public class ApiController {
 		TranslationDto selectedTranslation = i18nService.getTranslationByKeyIdAndLocale(translation);
 		
 		if(selectedTranslation == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
+			message = "Can not find any translation with keyId [" + keyId + "] and locale [" + locale + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.NOT_FOUND);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("translation", selectedTranslation);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
@@ -200,14 +256,22 @@ public class ApiController {
 		
 		logger.info("PUT /keys/{keyId}/translations/{locale}	keyId=" + keyId + ", locale=" + locale);
 		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String message = "";
+		
 		String detectedLocale = i18nService.getLanguageLocale(translation.getValue());
 		if(detectedLocale == null || !detectedLocale.equals(locale)) {
-			Map<String,Object> messageMap = new HashMap<String,Object>();
-			messageMap.put("message", "value and locale are not matched.");
+			message = "value [" + translation.getValue() + "] and locale [" + locale + "] are not matched.";
+			logger.info(message);
+			messageMap.put("message", message);
 			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.BAD_REQUEST);
 		}
 		
 		if(!validLocales.contains(locale)) {
+			message = "locale [" + locale + "] is not allowed. ['ko', 'en', 'ja'] are valid.";
+			logger.info(message);
+			messageMap.put("message", message);
 			return new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
 		}
 		
@@ -216,16 +280,21 @@ public class ApiController {
 		TranslationDto existingTranslation = i18nService.getTranslationByKeyIdAndLocale(translation);
 		
 		if(existingTranslation == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
+			message = "Can not find any translation with keyId [" + keyId + "] and locale [" + locale + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.NOT_FOUND);
 		}
 		
 		TranslationDto updatedTranslation = i18nService.updateTranslation(translation);
 		
 		if(updatedTranslation == null) {
-			return new ResponseEntity<Map<String,Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			message = "Failed to update Translation with keyId [" + keyId + "] and locale [" + locale + "].";
+			logger.info(message);
+			messageMap.put("message", message);
+			return new ResponseEntity<Map<String,Object>>(messageMap, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("translation", updatedTranslation);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
@@ -235,13 +304,19 @@ public class ApiController {
 	public ResponseEntity<Map<String,Object>> getLanguageLocale(@RequestParam(value="message", required=true)String message) {
 		
 		logger.info("GET /language_detect	message=" + message);
-		
+
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		Map<String,Object> messageMap = new HashMap<String,Object>();
+		String responseMessage = "";
+
 		String locale = i18nService.getLanguageLocale(message);
 		if(locale == null) {
+			responseMessage = "Can not detect locale of message [" + message + "]";
+			logger.info(responseMessage);
+			messageMap.put("message", responseMessage);
 			return new ResponseEntity<Map<String,Object>>(HttpStatus.NOT_FOUND);
 		}
 		
-		Map<String,Object> resultMap = new HashMap<String,Object>();
 		resultMap.put("locale", locale);
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
